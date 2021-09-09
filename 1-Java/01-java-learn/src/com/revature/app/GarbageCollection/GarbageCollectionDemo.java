ef3b2c6e9d26ca3669fa33d6f02dedbe36a614c7
S-4444 <sdas311999@gmail.com>
2021-09-09 13:24:49 +0530

    ProjectSetUp

---
+++ 1-Java/01-java-learn/src/com/revature/app/GarbageCollection/GarbageCollectionDemo.java
@@ -0,0 +1,10 @@
+package com.revature.app.GarbageCollection;
+
+public class GarbageCollectionDemo {
+
+	public static void main(String[] args) {
+		
+		Person person = new Person("jhon" , 15);
+	}
+
+}
---
+++ 1-Java/01-java-learn/src/com/revature/app/GarbageCollection/Person.java
@@ -0,0 +1,90 @@
+package com.revature.app.GarbageCollection;
+
+
+public class Person implements Comparable<Person> { 
+                    
+	private static long counter=0;
+	private int id ; 
+	private String name;
+	private int age;
+	
+
+	public Person(int id, String name, int age) {
+//		super();
+		this.id = counter++;
+		this.name = name;
+		this.age = age;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+   
+	@Override
+	protected void finalize() throws Throwable {
+		
+		super.finalize();
+		System.out.println(toString());
+	}
+
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public int compareTo(Person o) {
+		
+		return 0;
+	}
+	
+
+}
